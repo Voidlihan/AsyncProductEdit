8711f7106eb50d8c3feb975133fa0f2e410f001c
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-19 21:30:48 +0600

    Add project files.

---
+++ AsyncPrdouct/ProductTable.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AsyncPrdouct
+{
+    /// <summary>
+    /// Interaction logic for ProductTable.xaml
+    /// </summary>
+    public partial class ProductTable : Window
+    {
+        public ProductTable()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProduct(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditProduct(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
